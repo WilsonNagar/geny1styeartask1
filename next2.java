f8e9a23107ae86ed7b2a737abd36d14b2ec063ef
Wilson Nagar <41680139+WilsonNagar@users.noreply.github.com>
2020-10-01 12:11:11 +0530

    Next2 File
    Added Multiply and addition class file

---
+++ next2.java
@@ -0,0 +1,14 @@
+package winterproject;
+
+public class next2 {
+	public int mul(int a,int b) {
+		int c = a*b;
+		return c;
+		
+	}
+	public int add(int a,int b) {
+		int c = a+b;
+		return c;
+		
+	}
+}
